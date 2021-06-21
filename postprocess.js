426752f85a4e53850283ab7c5f755a61e06277bf
Jobe Diego Dylbas dos Santos <jobe.dylbas@inf.ufrgs.br>
2021-06-21 00:18:18 -0300

    bug: correct path for postprocess

--- postprocess.js
+++ postprocess.js
@@ -17,7 +17,7 @@ const csv = [
   header.join(','), // header row first
   ...items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
 ].join('\r\n')
-
+console.log(csv)
 // Step 3. Write a new CSV file with our data
 const newFilename = `incident-metrics.csv` // name of a new file to be saved
 await writeCSV(newFilename, csv) // create a new JSON file with just the Bitcoin price
