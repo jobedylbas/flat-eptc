92ebb32b4be1d4383b4c9ce0421f439861575d4d
Jobe Diego Dylbas dos Santos <jobe.dylbas@inf.ufrgs.br>
2021-06-21 00:13:38 -0300

    bug: add postprocess file

---
+++ postprocess.js
@@ -0,0 +1,23 @@
+// This can be a typescript file as well
+
+// Helper library written for useful postprocessing tasks with Flat Data
+// Has helper functions for manipulating csv, txt, json, excel, zip, and image files
+import { readJSON, writeCSV } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
+
+// Step 1: Read the downloaded_filename JSON
+const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
+const json = await readJSON(filename)
+console.log(json)
+
+// Step 2: Transform to CSV
+const items = json.items
+const replacer = (key, value) => value === null ? '' : value // specify how you want to handle null values here
+const header = Object.keys(items[0])
+const csv = [
+  header.join(','), // header row first
+  ...items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+].join('\r\n')
+
+// Step 3. Write a new CSV file with our data
+const newFilename = `incident-metrics.csv` // name of a new file to be saved
+await writeCSV(newFilename, csv) // create a new JSON file with just the Bitcoin price
